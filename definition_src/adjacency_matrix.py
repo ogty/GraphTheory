914439ad2692fef0387349a5722460c7995ca965
ogty <mofuiti1803@gmail.com>
2022-02-07 20:31:10 +0900

    add

---
+++ definition_src/adjacency_matrix.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+
+
+A = np.array([
+    [0, 1, 1, 1], 
+    [1, 0, 1, 0],
+    [1, 1, 0, 1],
+    [1, 0, 1, 0]
+])
+fig = plt.figure()
+tmp = nx.MultiGraph()
+G = nx.from_numpy_matrix(A, parallel_edges=True, create_using=tmp)
+nx.draw(G, with_labels=True)
+plt.show()
+fig.savefig("../img/graph_14.png")
